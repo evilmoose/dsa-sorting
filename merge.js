7aad961e6d3e2d41ecd3cd480639f37767f7d88c
Evilmoose <ronnielopez@gmail.com>
2024-05-23 15:25:02 -0700

    Merge and MergeSort

--- merge.js
+++ merge.js
@@ -1,5 +1,43 @@
 function merge() {}
 
-function mergeSort() {}
+function merge(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // Add remaining elements from arr1, if any
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    // Add remaining elements from arr2, if any
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
