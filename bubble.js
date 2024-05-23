6cda3ca9fcf435200951b45af3acb89f7f57bee1
Evilmoose <ronnielopez@gmail.com>
2024-05-23 15:17:50 -0700

    First commit

---
+++ bubble.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                noSwaps = false;
+            }
+        }
+        // If no swaps were made in the inner loop, the array is already sorted
+        if (noSwaps) break;
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
